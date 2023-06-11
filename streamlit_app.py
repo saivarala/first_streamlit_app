c914d380cd934086d3f25cab2bc4fd8cd5c47470
saivarala <44990790+saivarala@users.noreply.github.com>
2023-06-11 11:36:16 -0400

    Update streamlit_app.py
    normalized df

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,4 +29,4 @@ fruityvice_response = req.get("https://fruityvice.com/api/fruit/watermelon")
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
-streamlit.text(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
